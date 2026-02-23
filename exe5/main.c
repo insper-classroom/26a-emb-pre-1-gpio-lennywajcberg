ef29fb4002d826d39ec21998b72f4c626b7b5f7e
lennywajcberg <lennyw@al.insper.edu.br>
2026-02-23 12:35:01 -0300

    Ex 5 finalizado

--- exe5/main.c
+++ exe5/main.c
@@ -8,26 +8,29 @@ const int BTN2 = 7;
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN1); gpio_set_dir(BTN1, GPIO_IN); gpio_pull_up(BTN1);
-  gpio_init(BTN2); gpio_set_dir(BTN2, GPIO_IN); gpio_pull_up(BTN2);
+  gpio_init(BTN1); 
+  gpio_set_dir(BTN1, GPIO_IN); 
+  gpio_pull_up(BTN1);
+  gpio_init(BTN2); 
+  gpio_set_dir(BTN2, GPIO_IN); 
+  gpio_pull_up(BTN2);
+
 
   int cnt1 = 0, cnt2 = 0;
 
   while (1) {
-    if (gpio_get(BTN1) == 0) {
-      if (gpio_get(BTN1) == 0) {
+    if (!gpio_get(BTN1)) {
         printf("Botao 1: %d\n", ++cnt1);
-        while (gpio_get(BTN1) == 0) { }
         sleep_ms(20);
-      }
+        while (!gpio_get(BTN1))
+        sleep_ms(20);
     }
 
-    if (gpio_get(BTN2) == 0) {
-      if (gpio_get(BTN2) == 0) {
+    if (!gpio_get(BTN2)) {
         printf("Botao 2: %d\n", ++cnt2);
-        while (gpio_get(BTN2) == 0) { }
         sleep_ms(20);
-      }
+        while (!gpio_get(BTN2)) 
+        sleep_ms(200);
     }
   }
 }
