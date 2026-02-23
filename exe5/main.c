caba832ef138ed9967bcb3ab1ce7e225d3d19a33
lennywajcberg <lennyw@al.insper.edu.br>
2026-02-23 11:17:49 -0300

    ex5 e 6

--- exe5/main.c
+++ exe5/main.c
@@ -3,45 +3,33 @@
 #include "hardware/gpio.h"
 
 const int BTN1 = 26;
-const int BTN2 = 7;
+const int BTN2 = 28;
 
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN1);
-  gpio_set_dir(BTN1, GPIO_IN);
-  gpio_pull_up(BTN1);
-
-  gpio_init(BTN2);
-  gpio_set_dir(BTN2, GPIO_IN);
-  gpio_pull_up(BTN2);
+  gpio_init(BTN1); gpio_set_dir(BTN1, GPIO_IN); gpio_pull_up(BTN1);
+  gpio_init(BTN2); gpio_set_dir(BTN2, GPIO_IN); gpio_pull_up(BTN2);
 
   int cnt1 = 0, cnt2 = 0;
 
   while (1) {
-    // Botão 1
-    if (gpio_get(BTN1) == 0) {          // viu um aperto
-      sleep_ms(20);                    // espera “parar de vibrar”
-      if (gpio_get(BTN1) == 0) {       // confirma que ainda está apertado
-        cnt1++;
-        printf("Botao 1: %d\n", cnt1);
-
-        while (gpio_get(BTN1) == 0) { }  // evita quando segurar o botão e ficar contando, só conta quando soltar e apertar de novo
-        sleep_ms(20);                    // debounce da soltada
-      }    
-    }
-
-    // Botão 2
-    if (gpio_get(BTN2) == 0) {
+    if (gpio_get(BTN1) == 0) {
       sleep_ms(20);
-      if (gpio_get(BTN2) == 0) {
-        cnt2++;
-        printf("Botao 2: %d\n", cnt2);
-
-        while (gpio_get(BTN2) == 0) { }
-        sleep_ms(20);                  
+      if (gpio_get(BTN1) == 0) {
+        printf("Botao 1: %d\n", ++cnt1);
+        while (gpio_get(BTN1) == 0) { }
+        sleep_ms(20);
       }
     }
 
+    if (gpio_get(BTN2) == 0) {
+      sleep_ms(20);
+      if (gpio_get(BTN2) == 0) {
+        printf("Botao 2: %d\n", ++cnt2);
+        while (gpio_get(BTN2) == 0) { }
+        sleep_ms(20);
+      }
+    }
   }
 }
